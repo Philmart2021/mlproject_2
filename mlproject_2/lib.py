476eb3babae4b3747c0021be0afaa4b29a9cba89
Philippe Marque <philippemarque@hotmail.com>
2021-06-07 15:44:21 +0200

    add lib.py

---
+++ mlproject_2/lib.py
@@ -0,0 +1,8 @@
+def try_me (num1,num2):
+
+    # Add two numbers
+    sum = num1 + num2
+
+    # Display the sum
+    print('The sum of {0} and {1} is {2}. You have tried and succeeded to add 2 numbers'.format(num1, num2, sum))
+
